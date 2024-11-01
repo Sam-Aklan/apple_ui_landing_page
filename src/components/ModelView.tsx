517f9574c149c794e06c0e111b3135633e12bed8
Sam-Aklan <samaklan1998@gmail.com>
2024-11-01 18:45:13 +0300

    more

--- src/components/ModelView.tsx
+++ src/components/ModelView.tsx
@@ -44,9 +44,9 @@ const ModelView = ({
         enableZoom={false}
          enablePan={false}
          rotateSpeed={0.4}
-         //@ts-expect-error nothing
+         //@ts-ignore
          target={targetVector} 
-         //@ts-expect-error nothing
+         //@ts-ignore
          onEnd={()=> setRotation(controlRef?.current?.getAzimuthalAngle())}/>
         <group ref={()=>groupRef} name={`${index === 1?'small':'large'}`}
         position={[0,0,0]}>
